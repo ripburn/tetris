03db311d59ab852b1e05cac0d9e529a735a3b4fe
PC141237\ic141237 <yuya.m.0712@gmail.com>
2016-08-19 18:13:32 +0900

    first comit

---
+++ Assets/script/App_Touch.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class App_Touch : MonoBehaviour {
+	private static Vector3 TouchPosition = Vector3.zero;
+	private static Vector3 Position;
+	private static Vector3 Worldposition;
+
+
+	public enum TouchInfo{
+		None = 99,
+		Began = 0,
+		Moved = 1,
+		Stationary = 2,
+		Ended = 3,
+		Canceled = 4
+	}
+
+	public enum FlickInfo{
+		right = 0,
+		left = 1,
+		up = 2,
+		down = 3,
+		touch = 4,
+		none = 5
+	}
+
+	public static TouchInfo GetTouch(){
+		if (Application.isEditor) {
+			if (Input.GetMouseButtonDown (0)) {
+				return TouchInfo.Began;
+			}
+			if (Input.GetMouseButton (0)) {
+				return TouchInfo.Moved;
+			}
+			if (Input.GetMouseButtonUp (0)) {
+				return TouchInfo.Ended;
+			}
+		} else {
+			if (Input.touchCount > 0) {
+				return (TouchInfo)((int)Input.GetTouch (0).phase);
+			}
+		}
+		return TouchInfo.None;
+	}
+	public static Vector3 GetTouchPosition(){
+		if (Application.isEditor) {
+			TouchInfo touch = App_Touch.GetTouch ();
+			if (touch != TouchInfo.None) {
+				Position = Input.mousePosition;
+				Position.z = 20.0f;
+				Worldposition = Camera.main.ScreenToWorldPoint(Position);
+				return Worldposition;
+
+			}
+		} else {
+			if (Input.touchCount > 0) {
+				Touch touch = Input.GetTouch (0);
+				TouchPosition.x = touch.position.x;
+				TouchPosition.y = touch.position.y;
+				TouchPosition.z = 20.0f;
+				Worldposition = Camera.main.ScreenToWorldPoint(TouchPosition);
+				return Worldposition;
+			}
+		}
+		return Vector3.zero;
+	}
+	public static FlickInfo FlickDirection(Vector3 flickStartPos, Vector3 flickEndPos){
+		float directionX = flickEndPos.x - flickStartPos.x;
+		float directionY = flickEndPos.y - flickStartPos.y;
+
+		if (Mathf.Abs (directionY) < Mathf.Abs (directionX)) {
+			if (3 < directionX) {
+				return FlickInfo.right;
+			} else if (-3 > directionX) {
+				return FlickInfo.left;
+			}
+		} else if (Mathf.Abs (directionX) < Mathf.Abs (directionY)) {
+			if (3 < directionY) {
+				return FlickInfo.up;
+			} else if (-3 > directionY) {
+				return FlickInfo.down;
+			}
+		}
+		Debug.Log (directionX);
+		Debug.Log (directionY);
+
+		return FlickInfo.touch;
+	}
+
+
+
+}
+
+
+
+
---
+++ Assets/script/Group.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour {
+	float lastFall = 0;
+	private App_Touch.TouchInfo touch;
+	private Vector3 touchStartPos;
+	private Vector3 touchEndPos;
+	private App_Touch.FlickInfo flick = App_Touch.FlickInfo.none;
+	// Use this for initialization
+	void Start () {
+		if (!isValidGridPos ()) {
+			Debug.Log ("Game OVER");
+			Destroy (gameObject);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		touch = App_Touch.GetTouch ();
+		if (touch == App_Touch.TouchInfo.Began) {
+			touchStartPos = App_Touch.GetTouchPosition ();
+		}
+		if (touch == App_Touch.TouchInfo.Ended) {
+			touchEndPos = App_Touch.GetTouchPosition ();
+			flick = App_Touch.FlickDirection(touchStartPos, touchEndPos);
+			Debug.Log(flick);
+	
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) ||
+		    (flick == App_Touch.FlickInfo.left)) {
+			transform.position += new Vector3 (-1, 0, 0);
+			if (isValidGridPos ()) {
+				updateGrid ();
+			} else {
+				transform.position += new Vector3 (1, 0, 0);
+			}
+			flick = App_Touch.FlickInfo.none;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow) ||
+		           (flick == App_Touch.FlickInfo.right)) {
+			transform.position += new Vector3 (1, 0, 0);
+			if (isValidGridPos ()) {
+				updateGrid ();
+			} else {
+				transform.position += new Vector3 (-1, 0, 0);
+			}
+			flick = App_Touch.FlickInfo.none;
+		} else if (Input.GetKeyDown (KeyCode.UpArrow) ||
+		           (flick == App_Touch.FlickInfo.up)) {
+			transform.Rotate (0, 0, -90);
+			if (isValidGridPos ()) {
+				updateGrid ();
+			} else {
+				transform.Rotate (0, 0, 90);
+			}
+			flick = App_Touch.FlickInfo.none;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow) ||
+			(Time.time - lastFall >= 1) ||
+		           (flick == App_Touch.FlickInfo.down)) {
+			transform.position += new Vector3 (0, -1, 0);
+			if (isValidGridPos ()) {
+				updateGrid ();
+			} else {
+				transform.position += new Vector3 (0, 1, 0);
+				Grid.deleteFullRows ();
+				FindObjectOfType<Spawner> ().spawnNext ();
+				enabled = false;
+			}
+			lastFall = Time.time;
+			flick = App_Touch.FlickInfo.none;
+		}
+	}
+	bool isValidGridPos(){
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2 (child.position);
+
+			if (!Grid.insideBorder (v)) {
+				return false;
+			}
+			if (Grid.grid [(int)v.x, (int)v.y] != null &&
+				Grid.grid [(int)v.x, (int)v.y].parent != transform) {
+				return false;
+			}
+		}
+		return true;
+	}
+	void updateGrid(){
+		for (int y = 0; y < Grid.h; ++y) {
+			for (int x = 0; x < Grid.w; ++x) {
+				if (Grid.grid [x, y] != null) {
+					if (Grid.grid [x, y].parent == transform) {
+						Grid.grid [x, y] = null;
+					}
+				}
+			}
+		}
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2 (child.position);
+			Grid.grid [(int)v.x, (int)v.y] = child;
+		}
+	}
+}
